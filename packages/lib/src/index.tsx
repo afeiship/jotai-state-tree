33047eb35d017d3474f6614f80264f3bad781c28
afeiship <1290657123@qq.com>
2023-11-02 20:38:34 +0800

    feat: add get obj nested

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import { Provider, useStore, useAtom, createStore } from 'jotai';
 import '@jswork/next';
 
+const getv = (inTarget, inSuperKey?: string) => {
+  const keys = Object.keys(inTarget);
+  const result = {};
+  keys.forEach((key) => {
+    const k = inSuperKey ? `${inSuperKey}.${key}` : key;
+    nx.set(result, key, nx.$get(k));
+  });
+  return result;
+};
+
 const JotaiApp = (props: JotaiStateTreeProps) => {
   const store = useStore();
   const { stores } = props;
 
   nx.$get = (key?: string) => {
-    if (!key) return store;
+    if (!key) return getv(stores);
     const nameAtom = nx.get(stores, key);
-    return store.get(nameAtom);
+    try {
+      return store.get(nameAtom);
+    } catch {
+      return getv(nameAtom, key);
+    }
   };
 
   nx.$set = (key: string, value: any) => {
